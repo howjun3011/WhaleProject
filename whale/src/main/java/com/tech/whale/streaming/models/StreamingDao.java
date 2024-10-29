15220035ccf4c3f0c10eed191c36a7b0f5e4971b
IYUNKIM <queenasir0@gmail.com>
2024-10-29 20:40:21 +0900

    환경 설정 스트리밍 검색 기능 디비 작업 중

--- whale/src/main/java/com/tech/whale/streaming/models/StreamingDao.java
+++ whale/src/main/java/com/tech/whale/streaming/models/StreamingDao.java
@@ -4,5 +4,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface StreamingDao {
+	public Integer selectTrackId(String trackSpotifyId);
 	public void insertTrack(String trackArtist, String trackName, String trackAlbum, String trackCover, String trackSpotifyId);
 }

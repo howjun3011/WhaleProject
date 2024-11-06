23768c4060fa3faa091ae3579058afa603a293e8
IYUNKIM <queenasir0@gmail.com>
2024-11-06 18:12:59 +0900

    검색 기능 기본 마이바티스 구문 작성 완

--- whale/src/main/java/com/tech/whale/search/models/SearchDao.java
+++ whale/src/main/java/com/tech/whale/search/models/SearchDao.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.tech.whale.community.dto.PostDto;
+import com.tech.whale.feed.dto.FeedDto;
 import com.tech.whale.setting.dto.UserInfoDto;
 
 @Mapper
 public interface SearchDao {
 	public List<UserInfoDto> selectSearchUserInfo(String keyword);
 	public List<PostDto> selectSearchPostInfo(String keyword);
+	public List<FeedDto> selectSearchFeedInfo(String keyword);
 }

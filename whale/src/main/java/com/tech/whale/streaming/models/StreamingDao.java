d5198826b8edbf5685ce00eeeb49561fe02fab08
IYUNKIM <queenasir0@gmail.com>
2024-11-13 11:31:52 +0900

    트랙 카운트 수정
    트랙 카운트 수정

--- whale/src/main/java/com/tech/whale/streaming/models/StreamingDao.java
+++ whale/src/main/java/com/tech/whale/streaming/models/StreamingDao.java
@@ -7,11 +7,13 @@ import java.util.List;
 public interface StreamingDao {
 	public String selectTrackId(String trackSpotifyId);
 	public Integer selectTrackLikeId(String userId, String trackSpotifyId);
+	public Integer selectTrackCntId(String userId, String trackSpotifyId);
 	public TrackDto selectTrackDto(String trackSpotifyId);
 	public void insertTrack(String track_spotify_id, String track_artist, String track_name, String track_album, String track_cover);
 	public void insertTrackLike(String trackSpotifyId, String userId);
 	public void insertTrackCnt(String trackSpotifyId, String userId);
 	public void deleteTrackLike(Integer trackLikeId);
+	public void updateTrackCnt(Integer trackCntId);
 	public List<TrackDto> selectLikedTracks(String userId);
 	public Integer getLikeCountInfo(String userId);
 	public Integer getTrackLikeCountInfo(String userId, String trackSpotifyId);
